79a86ea9d6dcf482a63149842479cd99f0dc6b4c
ahugla <ahugla@vmware.com>
2020-01-08 17:43:30 +0100

    Update sendTrace-proxy.py

--- sendTrace-proxy.py
+++ sendTrace-proxy.py
@@ -14,80 +14,76 @@ from wavefront_opentracing_sdk.reporting import WavefrontSpanReporter
 import wavefront_sdk
 
 
-# pylint: disable=invalid-name
-if __name__ == '__main__':
-
-
-    application_tag = wavefront_sdk.common.ApplicationTags(application='ALEXapp',service='ALEXsvc')
+application_tag = wavefront_sdk.common.ApplicationTags(application='ALEXapp',service='ALEXsvc')
     
 
-    # Create Wavefront Span Reporter using Wavefront Proxy Client.
-    proxy_client = wavefront_sdk.WavefrontProxyClient(
+# Create Wavefront Span Reporter using Wavefront Proxy Client.
+proxy_client = wavefront_sdk.WavefrontProxyClient(
         host='localhost',
         tracing_port=30000,
-        distribution_port=2878,   #40000
+        distribution_port=2878,
         metrics_port=2878
-    )
-    proxy_reporter = WavefrontSpanReporter(client=proxy_client, source='ALEXH_tracing-example')
+)
+proxy_reporter = WavefrontSpanReporter(client=proxy_client, source='ALEXH_tracing-example')
 
 
-    # Create Composite reporter.
-    # Create Tracer with Composite Reporter.
-    # CompositeReporter takes a list of other reporters and invokes them one by one
-    # Use ConsoleReporter to output span data to console.
-    composite_reporter = CompositeReporter(proxy_reporter, ConsoleReporter())   
-    tracer = WavefrontTracer(reporter=composite_reporter, application_tags=application_tag)
+# Create Composite reporter.
+# Create Tracer with Composite Reporter.
+# CompositeReporter takes a list of other reporters and invokes them one by one
+# Use ConsoleReporter to output span data to console.
+composite_reporter = CompositeReporter(proxy_reporter, ConsoleReporter())   
+tracer = WavefrontTracer(reporter=composite_reporter, application_tags=application_tag)
 
 
-    global_tags = [('global_key', 'global_val')]
+global_tags = [('global_key', 'global_val')]
 
-    # Create span1, return a newly started and activated Scope.
-    scope = tracer.start_active_span(
+# Create span1, return a newly started and activated Scope.
+scope = tracer.start_active_span(
     	operation_name='span1',
         tags=global_tags,
         ignore_active_span=True,
         finish_on_close=True
-    )
-    span1 = scope.span
-    time.sleep(0.1)
+)
+span1 = scope.span
+time.sleep(0.1)
 
-    # Create span2, span3 child of span1.
-    span2 = tracer.start_span(
+# Create span2, span3 child of span1.
+span2 = tracer.start_span(
     	operation_name='span2',
         references=opentracing.child_of(span1.context),
         tags=[('span2_key', 'span2_val')]
-    )
-    span2.log_kv({'foo': 'bar'})
-    span3 = tracer.start_span(
+)
+span2.log_kv({'foo': 'bar'})
+span3 = tracer.start_span(
         operation_name='span3',
         child_of=span1,
         tags=[('span3_key', 'span3_val')]
-    )
-    time.sleep(0.2)
-    span2.finish()
-    time.sleep(0.1)
-    span3.finish()
+)
+time.sleep(0.2)
+span2.finish()
+time.sleep(0.1)
+span3.finish()
 
-    # Create span4 follows from span3.
-    span4 = tracer.start_span(
+# Create span4 follows from span3.
+span4 = tracer.start_span(
         operation_name='span4',
         references=opentracing.follows_from(span3.context),
         tags=[('span4_key', 'span4_val')]
-    )
-    time.sleep(0.2)
-    span4.finish()
+)
+time.sleep(0.2)
+span4.finish()
 
-    span5 = tracer.start_span(operation_name='span5')
-    time.sleep(0.1)
-    span5.finish()
+span5 = tracer.start_span(operation_name='span5')
+time.sleep(0.1)
+span5.finish()
 
-    time.sleep(0.1)
+time.sleep(0.1)
 
-    # Close the scope
-    scope.close()
+# Close the scope
+scope.close()
 
-    # Close the tracer
-    tracer.close()
+# Close the tracer
+tracer.close()
     
 
 
