b01b9ab064523bf0d2829c1a61348b8c9439fb6e
ahugla <ahugla@vmware.com>
2020-01-09 16:43:04 +0100

    Update testDT.php

--- testDT.php
+++ testDT.php
@@ -8,19 +8,6 @@
 			echo "Runs on : " . gethostname() . " (" . getHostByName(getHostName()) .")";
 
 
-			<!–– 
-			sleep(2)
-
-			echo "Tache 1";
-
-			sleep(2)
-			echo "Tache 2";
-
-			sleep(2)
-			echo "Tache 3";
-			––>
-
-
 
 		?>
 	</h7>
