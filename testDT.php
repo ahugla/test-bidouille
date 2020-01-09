1e7c82d9f3b795416368ee964fe7655ffa9e83f9
ahugla <ahugla@vmware.com>
2020-01-09 15:26:54 +0100

    Update testDT.php

--- testDT.php
+++ testDT.php
@@ -3,10 +3,12 @@
 
     <title>Test Distributed Tracing</title>
 
+    <h5>Test de DT ... running ... </h5>
+
 	<?php
 
 	// Show all information, defaults to INFO_ALL
-	phpinfo();
+	//phpinfo();
 
 	?>
 
