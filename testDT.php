80c1f870365115920a6852dd23b7a6616da5bf0f
ahugla <ahugla@vmware.com>
2020-01-09 16:46:58 +0100

    Update testDT.php

--- testDT.php
+++ testDT.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-    <title>Test Distributed Tracing</title>
-    <h5>Test de DT ... running ... </h5>
 
-	<h7>
-		<?php
-			echo "Runs on : " . gethostname() . " (" . getHostByName(getHostName()) .")";
+<title>Test Distributed Tracing</title>
+<h5>Test de DT ... running ... </h5>
+
+<h7>
+<?php
+	echo "Runs on : " . gethostname() . " (" . getHostByName(getHostName()) .")";
 
 
+	$command = escapeshellcmd('python3 /tmp/runSingleTest.sh.py');
+	$output = shell_exec($command);
+	echo $output;
 
-		?>
-	</h7>
+?>
+</h7>
 
 
 </html>
