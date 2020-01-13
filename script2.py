7307adef15bfab84549c0d33cebc5531b9cb15b1
ahugla <ahugla@vmware.com>
2020-01-13 11:26:04 +0100

    Update script2.py

--- script2.py
+++ script2.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
+$var2 = % (sys.argv[1])
+
 fichier = open("/var/www/html/script2.log", "a")
 fichier.write("execution de script2.py")
 fichier.close()
 
-message = 'script2.py completed !'
+message = 'script2.py completed ! avec variable: ' $var2
 print(message)
 
