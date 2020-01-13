a9d6aea8a76f2587f52262bc348d5e844a15a2d2
ahugla <ahugla@vmware.com>
2020-01-13 14:07:51 +0100

    Update script2.py

--- script2.py
+++ script2.py
@@ -5,11 +5,11 @@
 
 import sys
 
-var2='OK'
-print('var2 : ' +var2)
+#var2='OK'
+#print('var2 : ' +var2)
 
-var3=sys.argv[1]
-print('argument : ' +var3)
+#var3=sys.argv[1]
+#print('argument : ' +var3)
 
 fichier = open("/var/www/html/script2.log", "a")
 fichier.write("execution de script2.py")
