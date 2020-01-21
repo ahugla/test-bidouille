17dcf911b830310169c685a19987116fab186d18
ahugla <ahugla@vmware.com>
2020-01-21 10:43:05 +0100

    Update sendTraces.py

--- sendTraces.py
+++ sendTraces.py
@@ -8,6 +8,14 @@ import sys
 googleDelay=sys.argv[1]
 #print('argument : ' +googleDelay)
 
+
+
+fichier = open("/var/www/html/script2.log", "a")
+message = 'DEBUT'
+fichier.write(message)
+fichier.close()
+
+
 fichier = open("/var/www/html/script2.log", "a")
 message = 'googleDelay: ' +googleDelay
 fichier.write(message)
