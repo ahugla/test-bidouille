4d686c1b81100ed8eff48d89fc905cce16aed0a5
ahugla <ahugla@vmware.com>
2021-02-09 14:56:16 +0100

    adding test

---
+++ Python/test.py
@@ -0,0 +1,62 @@
+
+
+import paramiko     # pip3 install paramiko
+import time         # pas besoin de metttre en dependance car deja dans librairie par defaut
+
+#-----------------------------
+
+def executeSSHcommand(server, login, password, command):
+  client = paramiko.SSHClient()
+  client.set_missing_host_key_policy(paramiko.MissingHostKeyPolicy())    # pas de check de clé
+  client.connect(server, username=login, password=password)
+  ssh_stdin, ssh_stdout, ssh_stderr = client.exec_command(command)
+  # affichage de la sortie de la commande
+  for line in ssh_stdout:
+    print('... ' + line.strip('\n'))
+  client.close()
+  local_retour = "OK"
+  return local_retour
+
+#-----------------------------
+
+def wait_for_master():
+  time.sleep(5)  
+
+#----------------------------- 
+ 
+salt_master = "vrasaltstack.cpod-vrealize.az-fkd.cloud-garage.net"
+username="root"
+salt_master_password = "VMware1!"
+minion="vra-001111"
+#logs
+print("server salt master : " +salt_master)
+
+
+wait_for_master()
+print("fin du sleep")
+
+
+
+#On attent que le minion soit en etat "unregistered"
+retour="0"
+counter=0
+while (retour == "0") && (counter < 10):
+  # Creation de la commande
+  cmd_to_execute="salt-key --list=pre | grep " +minion +" | wc -l"
+  print("command to execute : " +cmd_to_execute)
+  # execution SSH
+  #retour=executeSSHcommand(salt_master,username,salt_master_password,cmd_to_execute)
+  print("retour: " +retour)
+  time.sleep(1) 
+  counter = counter + 1
+
+
+
+
+
+ 
+
+
+
+
+
