f4751e63518669049b995308dcfd6db895c45865
ahugla <ahugla@vmware.com>
2021-02-10 13:00:58 +0100

    Update test.py

--- Python/test.py
+++ Python/test.py
@@ -10,7 +10,7 @@ counter_max = 10 # nombre de tentavives max
 #salt_master = "vrasaltstack.cpod-vrealize.az-fkd.cloud-garage.net"
 salt_master = "10.11.10.29"        # A CHANGER
 username="root"
-#salt_master_password = "VMware1!"
+#salt_master_password = "VMware1!"   # A CHANGER
 salt_master_password = "changeme"
 
 #TEMP
@@ -27,10 +27,8 @@ def executeSSHcommand_FIND(server, login, password, command, minion):
   for line in ssh_stdout:
     trouve = line.find(minion)
     if trouve == 0:
-      print("TROUVE")
       local_retour = "TROUVE"
     else:
-      print("ABSENT")
       local_retour = "ABSENT"
   client.close()
   return local_retour
@@ -58,9 +56,9 @@ counter = 0
 while (retour == "ABSENT") and (counter < counter_max):
   # Creation de la commande
   # cmd_to_execute="salt-key --list=pre | grep " +minion +" | wc -l"
-  #cmd_to_execute="salt-key --list=pre | grep " +minion
+  cmd_to_execute="salt-key --list=pre | grep " +minion
+  #cmd_to_execute="salt-key --list-all | grep " +minion      # REMPLACER PAR  --list=pre
   print("Recherche du minion, tentative " +str(counter) +"/" +str(counter_max) +". Retry toutes les " +str(counter_sleep) +" secondes")
-  cmd_to_execute="salt-key --list-all | grep " +minion      # REMPLACER PAR  --list=pre
   print("command to execute : " +cmd_to_execute)
   # execution SSH
   retour=executeSSHcommand_FIND(salt_master,username,salt_master_password,cmd_to_execute, minion)
@@ -75,9 +73,9 @@ if retour == "TROUVE":
   print("Le minion a été trouvé en 'Unaccepted key' ")
   print("command to execute : " +cmd_to_execute)
   retour=executeSSHcommand_ACCEPT(salt_master,username,salt_master_password,cmd_to_execute)
-  print("Le minion a été trouvé accepté")
+  print("Le minion a été accepté")
 else: 
-  print("Le minion n'a  pas été trouvé !!")
+  print("Le minion n'a  pas été trouvé en 'Unaccepted Key'!!")
 
 
 
