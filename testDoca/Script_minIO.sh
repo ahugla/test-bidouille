45eeb16cf06fe2a1bc3eb4dcaba623bff0b2c54f
ahugla <ahugla@vmware.com>
2021-03-31 17:08:27 +0200

    Update Script_minIO.sh

--- testDoca/Script_minIO.sh
+++ testDoca/Script_minIO.sh
@@ -2,14 +2,13 @@
 
 
 
+# Recuperation des variables
+DB_password=$1
+echo "DB_password = " $DB_password
+
 
 cd /tmp
 yum install -y wget
-
-
-# add user and create conf dir
-useradd -s /sbin/nologin -d /opt/minio minio
-mkdir -p /opt/minio/bin
 
 
 #download minIO
@@ -19,17 +18,15 @@ chmod +x /opt/minio/bin/minio
 
 
 # folder for minio storage file
-mkdir -p /var/www/minio_storage/
-chown -R minio:minio /var/www/minio_storage/
-
+mkdir /data
 
 
 # minIO config
 cat >/opt/minio/minio.conf <<EOF
-MINIO_VOLUMES="/var/www/minio_storage/"
-MINIO_OPTS="--address :9100"
-MINIO_ACCESS_KEY= test
-MINIO_SECRET_KEY= changeme
+MINIO_VOLUMES="/data"
+MINIO_OPTS="--address :9000"
+MINIO_ACCESS_KEY=minioadmin
+MINIO_SECRET_KEY=$DB_password
 EOF
 
 
@@ -38,23 +35,35 @@ EOF
 cat >/etc/systemd/system/minio.service <<EOF
 [Unit]
 Description=Minio
-Documentation=https://docs.minio.io
 Wants=network-online.target
 After=network-online.target
 AssertFileIsExecutable=/opt/minio/bin/minio
+
 [Service]
-WorkingDirectory=/opt/minioUser=minio
-Group=minioPermissionsStartOnly=true
+WorkingDirectory=/root
+User=root
+Group=root
+
+PermissionsStartOnly=true
 EnvironmentFile=-/opt/minio/minio.conf
 ExecStartPre=/bin/bash -c "[ -n \"${MINIO_VOLUMES}\" ] || echo \"Variable MINIO_VOLUMES not set in /opt/minio/minio.conf\""
+
 ExecStart=/opt/minio/bin/minio server $MINIO_OPTS $MINIO_VOLUMES
+
 StandardOutput=journal
-StandardError=inherit# Specifies the maximum file descriptor number that can be opened by this process
-LimitNOFILE=65536# Disable timeout logic and wait until process is stopped
-TimeoutStopSec=0# SIGTERM signal is used to stop Minio
+StandardError=inherit
+
+# Specifies the maximum file descriptor number that can be opened by this process
+LimitNOFILE=65536
+
+# Disable timeout logic and wait until process is stopped
+TimeoutStopSec=0
+
+# SIGTERM signal is used to stop Minio
 KillSignal=SIGTERM
 SendSIGKILL=no
 SuccessExitStatus=0
+
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -63,54 +72,5 @@ EOF
 systemctl daemon-reload
 systemctl start minio
 systemctl enable minio
-
-
-
-
-# install nginx
-yum install -y nginx
-systemctl start nginx
-systemctl enable nginx
-
-
-# nginx conf
-cat >/etc/nginx/conf.d/minio.conf <<EOF
-server {
-    listen 80;
-    server_name  minio.domain.com www.minio.domain.com;    #this config will force through ssl / https
-    return 301 https://$server_name$request_uri;    root /var/www/html;
-    index index.html index.php;    access_log /var/log/nginx/minio-access.log;
-    error_log /var/log/nginx/minio-error.log;    location / {
-      proxy_pass          http://localhost:9100;
-      }
-   }server {
-     listen 443;
-     server_name minio.domain.com www.minio.domain.com;
-     ssl    on;
-        
-     #change max size upload file as you want
-     client_max_body_size 300M;
-        
-     ssl_certificate         /path/to/ssl/certificate;
-     ssl_certificate_key     /path/to/ssl/certificate_key;     ssl_session_cache shared:SSL:1m;
-     ssl_session_timeout  10m;
-     ssl_ciphers HIGH:!aNULL:!MD5;
-     ssl_prefer_server_ciphers on;     access_log   /var/log/nginx/minio-access.log;
-     error_log    /var/log/nginx/minio-error.log;     root /var/www/html;
-     index index.php;     location / {
-          proxy_pass          http://localhost:9100;
-          proxy_set_header    Host             $host;
-          proxy_set_header    X-Client-DN      $ssl_client_s_dn;
-          proxy_set_header    X-SSL-Subject    $ssl_client_s_dn;
-          proxy_set_header    X-SSL-Issuer     $ssl_client_i_dn;
-          proxy_read_timeout 1800;
-          proxy_connect_timeout 1800;
-          }
-}
-EOF
-
-# restart nginx
-systemctl restart nginx
-
 
 
